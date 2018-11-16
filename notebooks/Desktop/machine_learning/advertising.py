d87e2342fcc3a2fcadadc95a8e4fe663ecdfbe90
João Pablo <jp.pablolima@gmail.com>
2018-11-16 19:28:45 -0200

    aplicação do modelo de regressão linear

--- notebooks/Desktop/machine_learning/advertising.py
+++ notebooks/Desktop/machine_learning/advertising.py
@@ -14,3 +14,17 @@ x.shape
 
 y.shape
 
+#aplicação do modelo de regração linear
+import seaborn as sns 
+sns.pairplot(publi, x_vars=['TV','radio', 'newspaper'], y_vars='sales', size=7, kind='reg')
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
+
+#Importação do modelo de regressão linear
+
+from sklearn.linear_model import LinearRegression
+reglin = LinearRegression()
+reglin.fit(x_train, y_train)
+
+list(zip(['TV', 'radio', 'newspaper'], reglin.coef_))
